d5e338ec48f72646d2ccc5f12ea25a26e34f8ac2
PatsakornW <patsakorn.wtnsb@gmail.com>
2023-09-12 16:56:37 +0700

    fix

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,6 +10,7 @@ import flotilla from '../assets/flotilla.png'
 import thavorn from '../assets/thavorn.png'
 import fakestore from '../assets/fakestore.jpeg'
 import movie from '../assets/movie.jpeg'
+import weather from '../assets/weather.jpeg'
 
 
 import AOS from 'aos';
@@ -257,6 +258,27 @@ function Home() {
                             <div className='text-sm my-5'>
                                 <p>
                                 This website has been developed using React and is related to viewing details of movies and TV series. You can watch current trending movies and popular ones.
+                                </p>
+                            </div>
+                        </div>
+                        <div class=" px-6 pt-6 border border-black rounded-md flex flex-col md:h-[30rem]	 w-full md:w-80">
+                            <img class=" w-full h-48 rounded object-cover" src={weather} alt="weather" />
+                            <div class=" mt-4 mb-3 flex justify-between items-center">
+                                <div className='text-2xl font-bold tracking-tighter mb-1'>Weather</div>
+                                <div className='mx-1 w-3/12 h-[1px] bg-gray-200'></div>
+                                <div className='flex  gap-x-1'>
+                                    <a href="https://github.com/PatsakornW/weather" target='blank'> <img src={github} alt="github" className='w-6 h-6' /></a>
+                                    <a href="https://weather-woad-one.vercel.app/" target='blank'> <img src={web} alt="web" className='w-6 h-6' /></a>
+
+                                </div>
+                            </div>
+                            <div className='flex gap-x-2'>
+                                <div className="badge bg-gray-200 rounded">React</div>
+                                <div className="badge bg-gray-200 rounded">Tailwind</div>
+                            </div>
+                            <div className='text-sm my-5'>
+                                <p>
+                                This project fetches current and forecasted weather data from the OpenWeatherMap API. Users can search for countries or cities to access detailed weather information effortlessly.
                                 </p>
                             </div>
                         </div>
